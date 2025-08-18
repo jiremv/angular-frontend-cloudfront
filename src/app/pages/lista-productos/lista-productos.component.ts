6af171594a3b858811341f7d1b5ee08ea23d9071
jiremv <paulrivera34@gmail.com>
2025-08-18 00:45:02 -0600

    AWS Cloudfront Angular

--- src/app/pages/lista-productos/lista-productos.component.ts
+++ src/app/pages/lista-productos/lista-productos.component.ts
@@ -5,11 +5,13 @@ import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { ProductoService } from '../../services/producto.service';
 import { Producto } from '../../models/producto.model';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'app-lista-productos',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatButtonModule],
+  imports: [CommonModule, MatTableModule, MatButtonModule,  MatSelectModule,
+  MatFormFieldModule],
   templateUrl: './lista-productos.component.html',
   styleUrls: ['./lista-productos.component.scss']
 })
@@ -47,4 +49,9 @@ export class ListaProductosComponent implements OnInit {
   nuevo(): void {
     this.router.navigate(['/nuevo']);
   }
+
+  abrirRepositorio(url: string): void {
+    window.open(url, '_blank');
+  }
+
 }
